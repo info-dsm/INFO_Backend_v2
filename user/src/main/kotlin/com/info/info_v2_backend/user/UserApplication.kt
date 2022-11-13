b65094064ce08856d320dff89bc0ffadb76f2207
jinu <jinwoo794533@gmail.com>
2022-11-14 01:19:46 +0900

    Enable Kafka && Properties

--- user/src/main/kotlin/com/info/info_v2_backend/user/UserApplication.kt
+++ user/src/main/kotlin/com/info/info_v2_backend/user/UserApplication.kt
@@ -1,11 +1,15 @@
 package com.info.info_v2_backend.user
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableKafka
+@ConfigurationPropertiesScan
 class UserApplication
 fun main(args: Array<String>) {
     runApplication<UserApplication>(*args)

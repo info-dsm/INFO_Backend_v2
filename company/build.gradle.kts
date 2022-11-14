e13e859c2f0e629a0afd153b6c28221b0737dde7
jinu <jinwoo794533@gmail.com>
2022-11-14 20:48:39 +0900

    Company Build Gradle

---
+++ company/build.gradle.kts
@@ -0,0 +1,22 @@
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":user"))
+    implementation(project(":commonEntity"))
+    //MySQL driver
+    implementation("mysql:mysql-connector-java:8.0.31")
+    //Kafka
+    implementation("org.springframework.kafka:spring-kafka")
+    //Eureka Client
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.1.4")
+    //OpenFiegn
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.5")
+    //Hystrix
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-hystrix:2.2.10.RELEASE")
+    //Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.4")
+    //JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.5")
+    //Annotation Processor
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}

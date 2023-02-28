48a3b3e75afe37c99da63bfa1eb68dcf545bd3bc
jinu <jinwoo794533@gmail.com>
2023-03-01 04:15:09 +0900

    exception message default value

--- common/src/main/kotlin/com/info/info_v2_backend/common/exception/BusinessException.kt
+++ common/src/main/kotlin/com/info/info_v2_backend/common/exception/BusinessException.kt
@@ -3,7 +3,7 @@ package com.info.info_v2_backend.common.exception
 import com.info.info_v2_backend.common.logs.LogFormat
 
 open class BusinessException(
-    message: String?,
+    message: String? = null,
     val errorCode: ErrorCode
 ) : RuntimeException() {
     override val message: String? = message?.let {LogFormat.ERROR(it)}

12a008418d769f618c4e73e68e93366591b837e7
jinu <jinwoo794533@gmail.com>
2023-03-03 08:38:27 +0900

    선생님 companyNumber 권한 체크 오류

--- notice/src/main/kotlin/com/info/info_v2_backend/notice/adapter/input/rest/NoticeController.kt
+++ notice/src/main/kotlin/com/info/info_v2_backend/notice/adapter/input/rest/NoticeController.kt
@@ -129,7 +129,7 @@ class NoticeController(
         @RequestBody request: CreateNoticeRequest,
         @RequestParam companyNumber: String,
     ): PresignedUrlListResponse {
-        if (Auth.checkIsTeacher()) return createNoticeUsecase.create(Auth.checkCompanyNumber(companyNumber), request)
+        if (Auth.checkIsTeacher()) return createNoticeUsecase.create(companyNumber, request)
         return createNoticeUsecase.create(Auth.checkCompanyNumber(companyNumber), request)
     }
 

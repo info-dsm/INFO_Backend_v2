afce5b21f4aae868407470cc00c9bf5d486333b6
jinu <jinwoo794533@gmail.com>
2023-03-03 10:47:20 +0900

    set cloudflare feign client name

--- email/src/main/kotlin/com/info/info_v2_backend/email/adapter/output/rest/CloudflareFeignClient.kt
+++ email/src/main/kotlin/com/info/info_v2_backend/email/adapter/output/rest/CloudflareFeignClient.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@FeignClient(url = "https://api.mailchannels.net/tx/v1/send")
+@FeignClient(name = "cloudflare", url = "https://api.mailchannels.net/tx/v1/send")
 interface CloudflareFeignClient {
 
     @PostMapping

9c59f1ccd064e1a8752dc3c2a844635eafa05bea
jinu <jinwoo794533@gmail.com>
2023-03-07 09:48:10 +0900

    test

--- company/src/main/kotlin/com/info/info_v2_backend/company/application/service/RegisterCompany.kt
+++ company/src/main/kotlin/com/info/info_v2_backend/company/application/service/RegisterCompany.kt
@@ -127,7 +127,6 @@ class RegisterCompany(
                     )
                 )
             }
-
             
             saveCompanyPort.save(company)
             saveCompanyDocumentPort.save(

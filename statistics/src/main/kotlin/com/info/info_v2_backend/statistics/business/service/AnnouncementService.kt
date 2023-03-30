88f70cb1808029218cbcaf631298fb2b4cc034b0
jinu <jinwoo794533@gmail.com>
2023-03-30 14:33:02 +0900

    add filtering by announcementType feature

--- statistics/src/main/kotlin/com/info/info_v2_backend/statistics/business/service/AnnouncementService.kt
+++ statistics/src/main/kotlin/com/info/info_v2_backend/statistics/business/service/AnnouncementService.kt
@@ -1,6 +1,7 @@
 package com.info.info_v2_backend.statistics.business.service
 
 import com.info.info_v2_backend.common.file.dto.response.PresignedUrlListResponse
+import com.info.info_v2_backend.statistics.persistance.entity.announcement.AnnouncementType
 import com.info.info_v2_backend.statistics.presentation.dto.request.CreateAnnouncementRequest
 import com.info.info_v2_backend.statistics.presentation.dto.response.MaximumAnnouncementResponse
 import com.info.info_v2_backend.statistics.presentation.dto.response.MinimumAnnouncementResponse
@@ -10,7 +11,7 @@ interface AnnouncementService {
 
     fun createAnnouncment(request: CreateAnnouncementRequest): PresignedUrlListResponse
 
-    fun getAnnounceList(idx: Int, size: Int): Page<MinimumAnnouncementResponse>
+    fun getAnnounceList(idx: Int, size: Int, type: AnnouncementType?): Page<MinimumAnnouncementResponse>
     fun getAnnounce(id: Long): MaximumAnnouncementResponse
     fun getLatestAnnouncement(): MinimumAnnouncementResponse
 }

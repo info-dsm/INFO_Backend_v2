fecfabf629d266f42bf5d610705e80c0a0af5bf2
jinu <jinwoo794533@gmail.com>
2023-04-06 14:58:14 +0900

    edit query

--- notice/src/main/kotlin/com/info/info_v2_backend/notice/adapter/output/persistence/jpa/repository/NoticeRepository.kt
+++ notice/src/main/kotlin/com/info/info_v2_backend/notice/adapter/output/persistence/jpa/repository/NoticeRepository.kt
@@ -17,8 +17,8 @@ interface NoticeRepository: JpaRepository<Notice, String> {
     @Query(value = "select * from notice where notice_is_delete = false and company_number = :companyNumber", nativeQuery = true)
     fun findByCompanyNumber(@Param(value = "companyNumber") companyNumber: String): List<Notice>
 
-    @Query(value = "select n.* from notice n where n.notice_is_delete = false " +
-            "and n.company_number like %:companyName% " +
+    @Query(value = "select n.* from notice n " +
+            "where n.company_name like %:companyName% " +
             "and n.notice_is_delete = false " +
             "and curdate() between n.start_date and n.end_date " +
             "and n.notice_is_approve = 'APPROVE'",

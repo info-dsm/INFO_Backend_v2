512d5898518c4f68235edf5e6eedb3e71278f45b
jinu <jinwoo794533@gmail.com>
2023-05-05 03:04:45 +0900

    patch when class has no employment

--- employment/src/main/kotlin/com/info/info_v2_backend/employment/application/service/LoadEmployment.kt
+++ employment/src/main/kotlin/com/info/info_v2_backend/employment/application/service/LoadEmployment.kt
@@ -6,11 +6,14 @@ import com.info.info_v2_backend.common.exception.ErrorCode
 import com.info.info_v2_backend.employment.adapter.input.rest.dto.response.AnonymousEmploymentListResponse
 import com.info.info_v2_backend.employment.application.port.input.LoadEmploymentUsecase
 import com.info.info_v2_backend.employment.application.port.output.LoadEmploymentPort
+import com.info.info_v2_backend.employment.application.port.output.generation.LoadGenerationPort
+import com.info.info_v2_backend.employment.domain.student.FIRST_GENERATION_YEAR
 import org.springframework.stereotype.Service
 
 @Service
 class LoadEmployment(
-    private val loadEmploymentPort: LoadEmploymentPort
+    private val loadEmploymentPort: LoadEmploymentPort,
+    private val loadGenerationPort: LoadGenerationPort
 ): LoadEmploymentUsecase {
 
     override fun loadCompanyEmploymentLine(companyNumber: String): List<EmploymentDto> {
@@ -26,22 +29,23 @@ class LoadEmployment(
     }
 
     override fun loadAnonymousEmploymentListResponse(classNum: Int, year: Int): AnonymousEmploymentListResponse {
-        val employedStudentList = arrayListOf(loadEmploymentPort.loadEmploymentByClassNumAndYear(classNum, year).takeIf {
-            it.isNotEmpty()
-        }?: throw BusinessException(errorCode = ErrorCode.NO_DATA_FOUND_ERROR)).flatten().sortedByDescending {
+        val employedStudentList = arrayListOf(loadEmploymentPort.loadEmploymentByClassNumAndYear(classNum, year)).flatten().sortedByDescending {
             it.createdAt
         }.distinctBy {
             it.student.studentEmail
         }
 
+        val generationClass = loadGenerationPort.loadClass(classNum, year - FIRST_GENERATION_YEAR + 1)
+            ?: throw BusinessException(errorCode = ErrorCode.NO_DATA_FOUND_ERROR)
+
         return AnonymousEmploymentListResponse(
             classNum,
-            employedStudentList.first().generationClass.totalClassStudent,
+            generationClass.totalClassStudent,
             employedStudentList.size,
-            employedStudentList.first().generationClass.generationGrade.totalGradeStudent,
-            employedStudentList.first().generationClass.generationGrade.generationClassList.map {
-                generationClass ->
-                generationClass.employmentList.map {
+            generationClass.generationGrade.totalGradeStudent,
+            generationClass.generationGrade.generationClassList.map {
+                c ->
+                c.employmentList.map {
                     employment ->
                     employment.student.studentEmail
                 }
